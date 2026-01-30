252b242b82c3593c8d8d0134f6eb3d592edd25de
moham187 <moham187@OA-S141-18>
2026-01-30 18:06:47 -0500

    DONE

--- Assets/Week 4/Flipper.cs
+++ Assets/Week 4/Flipper.cs
@@ -1,28 +1,27 @@
+
 using UnityEngine;
 
 public class Flipper : MonoBehaviour
 {
     public float speed;
-    //stored boolean that checks if button is pressed.
     private bool isMoving = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if()
+        if (isMoving == true)
         {
             transform.position += Vector3.right * speed * Time.deltaTime;
         }
-        
-
-      
 
     }
+
     public void OnMoveClick()
     {
         isMoving = true;
@@ -32,4 +31,6 @@ public class Flipper : MonoBehaviour
     {
         isMoving = false;
     }
+
+
 }
